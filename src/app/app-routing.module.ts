637e8f2ab6bbdd77f75919a9cc01a5e2dba5e8f5
VladDyrenkov <59515329+VladDyrenkov@users.noreply.github.com>
2022-01-21 19:04:10 +0200

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryDishesComponent } from './bloks/main/category-dishes/category-dishes.component';
+import { CategoryComponent } from './bloks/main/category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'categories',
+    loadChildren: () => import('./bloks/main/category/category.module').then((m) => m.CategoryModule),
+  },
+  {
+    path: '',
+    redirectTo: 'categories',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './bloks/header/header.component';
+import { FooterComponent } from './bloks/footer/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BasketComponent } from './bloks/footer/basket/basket.component';
+import { SharedModule } from './bloks/reusable_components/shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasketComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
